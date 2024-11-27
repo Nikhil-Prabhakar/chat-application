233b7c452dd409ad6f3fb9b48cde620777c9768c
Nikhil Prabhakar <prabhakarnikhil2003@gmail.com>
2024-11-27 23:39:23 +0530

    Error Occurred

--- client/src/pages/chat/components/chat-container/components/message-container/index.jsx
+++ client/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -68,7 +68,19 @@ const MessageContainer = () => {
     });
   };
 
-  const donwloadFile = (file) => {};
+  const donwloadFile = async (file) => {
+    const response = await apiClient.get(`${HOST}/${url}`, {
+      responseType: "blob",
+    });
+    const urlBlob = window.URL.createObjectURL(new Blob([response.data]));
+    const link = document.createElement("a");
+    link.href = urlBlob;
+    link.setAttribute("download", url.split("/").pop());
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+    window.URL.revokeObjectURL(urlBlob);
+  };
 
   const renderDMMessages = (message) => (
     <div
